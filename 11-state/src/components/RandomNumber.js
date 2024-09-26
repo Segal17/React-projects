8c5f13bcaa981436503ca1377ab9f7732e59ea67
Segal17 <74028860+Segal17@users.noreply.github.com>
2024-09-26 15:32:09 +0300

    update 11 project
    update 11 project

--- 11-state/src/components/RandomNumber.js
+++ 11-state/src/components/RandomNumber.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import generateRandomNum from "../utils/generateRandomNum";
 
-function RandomNumber() {
-  const [randomNum, setRandomNum] = useState(generateRandomNum());
+function RandomNumber(props) {
+  console.log("rendered");
+  const { maxNum } = props;
+  const [randomNum, setRandomNum] = useState(generateRandomNum(maxNum));
   const changeRandomNum = () => {
-    setRandomNum(generateRandomNum());
+    setRandomNum(generateRandomNum(maxNum));
     console.log(randomNum);
   };
   return (
